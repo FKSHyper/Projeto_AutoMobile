f99070aa8675e580f950491074953d08fb4d7c31
martimmmmmmm23 <martim.jacob.t0133602@edu.atec.pt>
2026-05-06 17:12:31 +0100

    Criada a classe clientes.

---
+++ Data/Projeto_AutoMobile/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace Projeto_AutoMobile.Data.Projeto_AutoMobile
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string NIF { get; set; }
+    }
+}
