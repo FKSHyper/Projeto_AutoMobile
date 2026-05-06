86850572828122fe9f5de849fe8c924665fd0470
martimmmmmmm23 <martim.jacob.t0133602@edu.atec.pt>
2026-05-06 17:33:30 +0100

    Classe Clientes atualizada

---
+++ Data/Projeto_AutoMobile/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Projeto_AutoMobile.Data.Projeto_AutoMobile
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NIF { get; set; }
+        public string CartaConducao { get; set; }
+        public string Email { get; set; }
+        public string Telemovel { get; set; }
+    }
+}
