06de9a2eef350c89464d024bd2d30fa79962cfc3
martimmmmmmm23 <martim.jacob.t0133602@edu.atec.pt>
2026-05-11 19:50:23 +0100

    Controller alterado

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projeto_AutoMobile.Data;
 using Projeto_AutoMobile.Data.Projeto_AutoMobile;
+using Projeto_AutoMobile.ViewModels.Clientes;
 
 namespace Projeto_AutoMobile.Controllers
 {
@@ -18,101 +17,105 @@ namespace Projeto_AutoMobile.Controllers
             _context = context;
         }
 
-        // GET: Clientes
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Clientes.ToListAsync());
+            var listaClientes = _context.Clientes;
+            return View(await listaClientes.ToListAsync());
         }
 
-        // GET: Clientes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var cliente = await _context.Clientes
                 .FirstOrDefaultAsync(m => m.Id == id);
 
-            if (cliente == null)
-            {
-                return NotFound();
-            }
+            if (cliente == null) return NotFound();
+
+            ViewBag.SelectedId = id;
 
             return View(cliente);
         }
 
-        // GET: Clientes/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Clientes/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Nome,NIF,CartaConducao,Email,Telemovel")] Cliente cliente)
+        public async Task<IActionResult> Create(ClienteViewModel model)
         {
             if (ModelState.IsValid)
             {
+                var cliente = new Cliente
+                {
+                    Nome = model.Nome,
+                    NIF = model.NIF,
+                    CartaConducao = model.CartaConducao,
+                    Email = model.Email,
+                    Telemovel = model.Telemovel
+                };
+
                 _context.Add(cliente);
                 await _context.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(cliente);
+            return View(model);
         }
 
-        // GET: Clientes/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var cliente = await _context.Clientes.FindAsync(id);
 
-            if (cliente == null)
-            {
-                return NotFound();
-            }
+            if (cliente == null) return NotFound();
 
-            return View(cliente);
+            ViewBag.SelectedId = id;
+
+            var viewModel = new ClienteViewModel
+            {
+                Nome = cliente.Nome,
+                NIF = cliente.NIF,
+                CartaConducao = cliente.CartaConducao,
+                Email = cliente.Email,
+                Telemovel = cliente.Telemovel
+            };
+
+            return View(viewModel);
         }
 
-        // POST: Clientes/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Nome,NIF,CartaConducao,Email,Telemovel")] Cliente cliente)
+        public async Task<IActionResult> Edit(int id, ClienteViewModel model)
         {
-            var clienteBD = await _context.Clientes.FindAsync(id);
-
-            if (clienteBD == null)
-            {
+            if (!_context.Clientes.Select(x => x.Id).Contains(id))
                 return NotFound();
-            }
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    clienteBD.Nome = cliente.Nome;
-                    clienteBD.NIF = cliente.NIF;
-                    clienteBD.CartaConducao = cliente.CartaConducao;
-                    clienteBD.Email = cliente.Email;
-                    clienteBD.Telemovel = cliente.Telemovel;
+                    var cliente = await _context.Clientes.FindAsync(id);
 
+                    if (cliente == null) return NotFound();
+
+                    cliente.Nome = model.Nome;
+                    cliente.NIF = model.NIF;
+                    cliente.CartaConducao = model.CartaConducao;
+                    cliente.Email = model.Email;
+                    cliente.Telemovel = model.Telemovel;
+
+                    _context.Update(cliente);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ClienteExists(id))
-                    {
+                    if (!_context.Clientes.Any(e => e.Id == id))
                         return NotFound();
-                    }
 
                     throw;
                 }
@@ -120,29 +123,24 @@ namespace Projeto_AutoMobile.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(cliente);
+            ViewBag.SelectedId = id;
+            return View(model);
         }
 
-        // GET: Clientes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var cliente = await _context.Clientes
                 .FirstOrDefaultAsync(m => m.Id == id);
 
-            if (cliente == null)
-            {
-                return NotFound();
-            }
+            if (cliente == null) return NotFound();
+
+            ViewBag.SelectedId = id;
 
             return View(cliente);
         }
 
-        // POST: Clientes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -157,11 +155,6 @@ namespace Projeto_AutoMobile.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool ClienteExists(int id)
-        {
-            return _context.Clientes.Any(e => e.Id == id);
         }
     }
 }
