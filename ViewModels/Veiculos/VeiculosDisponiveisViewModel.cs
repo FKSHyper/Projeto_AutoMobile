601f7235fd4ef631a645333d442658863a25792a
Hyper <128801212+FKSHyper@users.noreply.github.com>
2026-05-11 21:14:07 +0100

    Filtrar veiculos disponiveis Visivel em /Veiculos/Disponiveis

---
+++ ViewModels/Veiculos/VeiculosDisponiveisViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Projeto_AutoMobile.Data.Projeto_AutoMobile;
+
+namespace Projeto_AutoMobile.ViewModels.Veiculos
+{
+    public class VeiculosDisponiveisViewModel
+    {
+        // A data do teu simulador
+        public DateTime DataAtual { get; set; }
+
+        // Guarda o tipo que o utilizador escolheu no filtro
+        public string TipoSelecionado { get; set; }
+
+        // A lista de veículos filtrada que vai aparecer na tabela
+        public List<Veiculo> Veiculos { get; set; }
+
+        public VeiculosDisponiveisViewModel()
+        {
+            Veiculos = new List<Veiculo>();
+        }
+    }
+}
